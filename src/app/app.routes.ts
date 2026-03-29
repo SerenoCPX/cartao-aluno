c6b63024d56753af1f86ba7fa075f7d55783f929
Rafael Reis <rafa.reis20032003@gmail.com>
2026-03-28 22:27:43 -0300

    Projeto ionic final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'sobre',
+    loadComponent: () => import('./sobre/sobre.page').then( m => m.SobrePage)
+  },
+  {
+    path: 'sobre',
+    loadComponent: () => import('./sobre/sobre.page').then( m => m.SobrePage)
+  },
 ];
---
+++ src/app/sobre/sobre.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+    RouterLink,
+    CommonModule,
+    FormsModule
+  ]
+})
+export class SobrePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
